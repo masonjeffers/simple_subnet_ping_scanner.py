886539912f0b4bbbbf18e88d1adda5cf3cf73632
Mason Jeffers <masonjeffers@gmail.com>
2020-05-18 12:48:26 -0400

    Update ping_sweep.py

--- ping_sweep.py
+++ ping_sweep.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 import os
 subnet=raw_input("Type first three octets of subnet to scan (such as \"192.168.0\":")
-for x in range(1,5):
+for x in range(1,254):
   cmd="ping -c1 " + subnet + "." + str(x) + " > /dev/null"
   res=os.system(cmd)
   if res == 0:
